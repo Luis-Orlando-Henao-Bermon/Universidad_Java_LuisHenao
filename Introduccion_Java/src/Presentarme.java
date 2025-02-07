bdcceb19a8da24754cf878e9e8b7e4dbef717ade
Luis <roronoazoro3704@gmail.com>
2025-02-07 07:51:45 -0500

    feat :sparkles: my first commit

---
+++ Introduccion_Java/src/Presentarme.java
@@ -0,0 +1,9 @@
+//Tarea 1 Presentarme
+
+public class Presentarme {
+    public static void main(String[] args) {
+        System.out.println("Luis Orlando Henao Bermon");
+        System.out.println("Tengo 20 años");
+        System.out.println("Soy de colombia");
+    }
+}
---
+++ Variables/src/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        //variables
+        int edad = 31;
+        double pi = 3.1416;
+        boolean disponible = true;
+        char genero;
+        genero = 'M';
+        String nombre = "Luis Henao";
+        System.out.println(pi);
+        System.out.println(edad);
+        System.out.println(disponible);
+        System.out.println(genero);
+        System.out.println(nombre);
+
+    }
+}
